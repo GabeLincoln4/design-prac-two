4367cc257b658be4eb7ca9578bc627606b0945ad
Gavin <7797864896@K12.LCS>
2022-09-20 17:50:23 -0400

    Refine edit button equation

--- src/Components/Buttons.jsx
+++ src/Components/Buttons.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 
-function EditButtons(numberOfColumns, buttonStartPoint){
+function EditButtons(props){
     return (
-        <div className={"grid grid-rows-3 grid-cols-" + (numberOfColumns) + " h-screen"}>
-            <div className={"bg-red-500 col-start-" + (buttonStartPoint) + " row-start-1 text-white"}>01</div>
-            <div className={"bg-green-500 col-start-" + (buttonStartPoint) + " row-start-2 text-white"}>02</div>
-            <div className={"bg-blue-500 col-start-" + (buttonStartPoint) + " row-start-3 text-white"}>03</div>
+        <div className={"grid grid-rows-3" + (props.numOfCols) + " h-screen"}>
+            <div className={"bg-red-500 "+ (props.startPoint) + " row-start-1 text-white"}>01</div>
+            <div className={"bg-green-500 " + (props.startPoint) + " row-start-2 text-white"}>02</div>
+            <div className={"bg-blue-500 " + (props.startPoint) + " row-start-3 text-white"}>03</div>
         </div>
-    )
+    );
 }
 
 function Buttons(){
 
-    const numOfCols = 4;
-    const startPoint = 4;
+    const numOfCols = "grid-cols-5";
+    const startPoint = "col-start-5";
 
     return (
         <div>
-            {EditButtons(numOfCols, startPoint)}
+            <EditButtons numOfCols={numOfCols} startPoint={startPoint} />
         </div>
     );
 };
