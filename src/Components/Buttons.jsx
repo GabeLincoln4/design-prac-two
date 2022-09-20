fc86fd8e915f073faba1ca65f885d075585d739a
Gavin <7797864896@K12.LCS>
2022-09-20 16:41:11 -0400

    Create function that allows for easy editing of side buttons

--- src/Components/Buttons.jsx
+++ src/Components/Buttons.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 
-function Buttons(){
+function EditButtons(numberOfColumns, buttonStartPoint){
     return (
-        <div>
-            <div className="grid grid-rows-3 grid-cols-3 h-screen">
-                <div className="bg-red-500 col-start-3 row-start-1 text-white">01</div>
-                <div className="bg-green-500 col-start-3 row-start-2 text-white">02</div>
-                <div className="bg-blue-500 col-start-3 row-start-3 text-white">03</div>
-            </div>
+        <div className={"grid grid-rows-3 grid-cols-" + (numberOfColumns) + " h-screen"}>
+            <div className={"bg-red-500 col-start-" + (buttonStartPoint) + " row-start-1 text-white"}>01</div>
+            <div className={"bg-green-500 col-start-" + (buttonStartPoint) + " row-start-2 text-white"}>02</div>
+            <div className={"bg-blue-500 col-start-" + (buttonStartPoint) + " row-start-3 text-white"}>03</div>
         </div>
     )
+}
+
+function Buttons(){
+
+    const numOfCols = 4;
+    const startPoint = 4;
+
+    return (
+        <div>
+            {EditButtons(numOfCols, startPoint)}
+        </div>
+    );
 };
 
 export default Buttons;
